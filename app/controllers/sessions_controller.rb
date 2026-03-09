ca64c3b05d9dc35a2aaad301a91e9c8df854cf6d
avery-joo <sohyeong.joo@kellogg.northwestern.edu>
2026-03-09 20:20:43 +0000

    Login/Logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,28 @@ class SessionsController < ApplicationController
   end
 
   def create
+     @user = User.find_by({"email" => params["email"]})
+     if @user != nil # 1. when user exists
+      if BCrypt::Password.new(@user["password"]) == params["password"] # 1-1. when password is correct
+        cookies["zebra"] = "giraffe"
+        session["user_id"] = @user["id"]
+
+        flash["notice"] = "Welcome."
+        redirect_to "/places"
+      else
+        flash["notice"] = "Incorrect password." # 1-2. when password is incorrect
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Email not found." # 2. when user doesn't exist
+      redirect_to "/login"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Goodbye."
+    redirect_to "/login"
   end
 end
   
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources "entries"
   resources "places"
   resources "sessions"
-  resources "users"
+    get("/login", {:controller => "sessions", :action => "new"})
+    get("/logout", {:controller => "sessions", :action => "destroy"})
+  resources "users", only: [:new, :create]
 end
